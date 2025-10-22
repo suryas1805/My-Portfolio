c5f0e2aa609d21c5afd2cc9da1db1a2562fd6d8e
suryas-1805 <suryas18051998@gmail.com>
2025-10-22 09:58:15 +0530

    frontend base url added

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors({
-    origin: ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:5175'],
+    origin: ['https://suryas1805-portfolio.netlify.app'],
     // origin: '*',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
