be467993c0599763c4ab1d74bd7c027b7c803ac5
suryas-1805 <suryas18051998@gmail.com>
2025-11-18 17:32:09 +0530

    new update

--- backend/utils/mailer.js
+++ backend/utils/mailer.js
@@ -5,6 +5,7 @@ import nodemailer from 'nodemailer';
 export const sendMail = async (to, subject, html) => {
 	const transporter = nodemailer.createTransport({
 		service: 'Gmail',
+		secure: false,
 		auth: {
 			user: process.env.EMAIL,
 			pass: process.env.EMAIL_PASS,
